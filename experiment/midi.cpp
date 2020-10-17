751009b039f4db4712c1f8572b4b93cb56beb9f3
Ronan Keryell <ronan@keryell.fr>
2020-10-17 14:37:01 -0700

    Modernize MIDI example and add input display

--- experiment/midi.cpp
+++ experiment/midi.cpp
@@ -1,61 +1,77 @@
 #include "rtmidi/RtMidi.h"
-// midiprobe.cpp
-#include <iostream>
-#include <cstdlib>
 
-int main()
-{
-//  RtMidiIn  *midiin = 0;
-//  RtMidiOut *midiout = 0;
-  RtMidiIn  midiin;
-  RtMidiOut midiout;
+#include <chrono>
+#include <csignal>
+#include <cstdint>
+#include <cstdlib>
+#include <iostream>
+#include <thread>
+#include <vector>
+
+// To use time unit literals directly
+using namespace std::chrono_literals;
+
+namespace {
+
+volatile bool done = false;
+
+}
+
+auto check_error = [] (auto&& function) {
+  try {
+    return function();
+  }
+  catch (const RtMidiError &error) {
+    error.printMessage();
+    std::exit(EXIT_FAILURE);
+  }
+};
+
+int main() {
   // RtMidiIn constructor
-  try {
-//    midiin = new RtMidiIn();
+  auto midi_in = check_error([] { return RtMidiIn {}; });
+
+  // Check inputs
+  auto n_in_ports = midi_in.getPortCount();
+  std::cout << "\nThere are " << n_in_ports
+            << " MIDI input sources available.\n";
+
+  for (auto i = 0; i < n_in_ports; ++i) {
+    auto port_name = check_error([&] { return midi_in.getPortName(i); });
+    std::cout << "  Input Port #" << i << ": " << port_name << '\n';
   }
-  catch ( RtMidiError &error ) {
-    error.printMessage();
-    exit( EXIT_FAILURE );
-  }
-  // Check inputs.
-  unsigned int nPorts = midiin.getPortCount();
-  std::cout << "\nThere are " << nPorts << " MIDI input sources available.\n";
-  std::string portName;
-  for ( unsigned int i=0; i<nPorts; i++ ) {
-    try {
-      portName = midiin.getPortName(i);
-    }
-    catch ( RtMidiError &error ) {
-      error.printMessage();
-      goto cleanup;
-    }
-    std::cout << "  Input Port #" << i+1 << ": " << portName << '\n';
-  }
+
   // RtMidiOut constructor
-  try {
-//    midiout = new RtMidiOut();
-  }
-  catch ( RtMidiError &error ) {
-    error.printMessage();
-    exit( EXIT_FAILURE );
-  }
+  auto midi_out = check_error([] { return RtMidiOut {}; });
+
   // Check outputs.
-  nPorts = midiout.getPortCount();
-  std::cout << "\nThere are " << nPorts << " MIDI output ports available.\n";
-  for ( unsigned int i=0; i<nPorts; i++ ) {
-    try {
-      portName = midiout.getPortName(i);
-    }
-    catch (RtMidiError &error) {
-      error.printMessage();
-      goto cleanup;
-    }
-    std::cout << "  Output Port #" << i+1 << ": " << portName << '\n';
+  auto n_out_ports = midi_out.getPortCount();
+  std::cout << "\nThere are " << n_out_ports
+            << " MIDI output ports available.\n";
+  for (auto i = 0; i < n_out_ports; ++i) {
+    auto port_name = check_error([&] { return midi_out.getPortName(i); });
+    std::cout << "  Output Port #" << i << ": " << port_name << '\n';
   }
-  std::cout << '\n';
-  // Clean up
- cleanup:
-//  delete midiin;
-//  delete midiout;
+  std::cout << std::endl;
+
+  midi_in.openPort(1);
+  // Don't ignore sysex, timing, or active sensing messages
+  midi_in.ignoreTypes(false, false, false);
+
+  std::cout << "Reading MIDI from port ... quit with Ctrl-C.\n";
+  std::signal(SIGINT, [] (int) { done = true; });
+  std::vector<std::uint8_t> message;
+  while (!done) {
+    auto stamp = midi_in.getMessage(&message);
+    auto n_bytes = message.size();
+    for (int i = 0; i < n_bytes; ++i)
+      std::cout << "Byte " << i << " = " << static_cast<int>(message[i])
+                << ", ";
+    if (n_bytes > 0)
+      std::cout << "stamp = " << stamp << std::endl;
+    // Sleep for 10 milliseconds ... platform-dependent.
+    std::this_thread::sleep_for(10ms);
+  }
+  std::cout << "Done!" << std::endl;
   return 0;
 }
