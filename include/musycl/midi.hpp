3c8efdb4d0d4eb6efd94beec0c8f6fd738109d17
Ronan Keryell <ronan@keryell.fr>
2020-11-01 10:28:53 -0800

    Add MIDI message narrowing constructor to handle signed values

--- include/musycl/midi.hpp
+++ include/musycl/midi.hpp
@@ -10,18 +10,42 @@
 namespace musycl::midi {
 
 /// A "note" MIDI message implementation detail
-class note {
+class note_base {
 public :
+  /* Use signed integers because it is less disturbing when doing
+     arithmetic on values */
+
+  /// The channel number between 0 and 15
   std::int8_t channel;
+
+  /// The note number between 0 and 127, C3 note is 60
   std::int8_t note;
+
+  /// The note velocity between 0 and 127
   std::int8_t velocity;
+
+  note_base() = default;
+
+  /// A specific constructor to handle unsigned to signed narrowing
+  template <typename Channel, typename Note, typename Velocity>
+  note_base(Channel&& c, Note&& n, Velocity&& v)
+    : channel { static_cast<int8_t>(c) }
+    , note { static_cast<int8_t>(n) }
+    , velocity { static_cast<int8_t>(v) }
+  {}
 };
 
 /// A "note on" MIDI message is just a kind of note
-class on : public note {};
+class on : public note_base {
+  /// Inherit the note_base constructors
+  using note_base::note_base;
+};
 
 /// A "note off" MIDI message is just a kind of note
-class off : public note {};
+class off : public note_base {
+  /// Inherit the note_base constructors
+  using note_base::note_base;
+};
 
 /** A MIDI message can be one of different types, including the
     monostate for empty message at initialization */
