6f0fa28df4a6d1be2f3b7054dc100d87c9efa28b
Ronan Keryell <ronan@keryell.fr>
2020-11-14 11:31:48 -0800

    Use high_status parser

--- include/musycl/midi.hpp
+++ include/musycl/midi.hpp
@@ -1,7 +1,14 @@
 #ifndef MUSYCL_MIDI_HPP
 #define MUSYCL_MIDI_HPP
 
-/// \file Abstractions for MIDI messages
+/** \file Abstractions for MIDI messages
+
+    Some MIDI background information can be found on
+    https://www.midi.org/ (free registration) in documents such as the
+    "MIDI 1.0 Detailed Specification", Document Version 4.2.1, Revised
+    February 1996
+    https://www.midi.org/specifications/midi1-specifications/m1-v4-2-1-midi-1-0-detailed-specification-96-1-4
+*/
 
 #include <cstdint>
 #include <string>
@@ -51,18 +58,29 @@ class off : public note_base {
     monostate for empty message at initialization */
 using msg = std::variant<std::monostate, midi::on, midi::off>;
 
+
+/// Get the 4 MSB bits of the MIDI status byte that give the command kind
+static inline std::int8_t high_status(std::uint8_t first_byte) {
+  return first_byte>>4;
+}
+
+
 /// Parse a MIDI byte message into a specific MIDI instruction
 msg parse(const std::vector<std::uint8_t>& midi_message) {
   msg m;
-  /// Interesting MIDI messages have 3 bytes
-  if (midi_message.size() == 3) {
-    if (midi_message[0] == 144 && midi_message[2] != 0)
-      // Start the note
-      m = midi::on { 0, midi_message[1], midi_message[2] };
-    else if (midi_message[0] == 128
-             || (midi_message[0] == 144 && midi_message[2] == 0))
-      // Stop the note
-      m = midi::off { 0, midi_message[1], midi_message[2] };
+  if (!midi_message.empty()) {
+    auto hs = high_status(midi_message[0]);
+    /// Interesting MIDI messages have 3 bytes
+    if (midi_message.size() == 3) {
+      if (hs == 9 && midi_message[2] != 0)
+        // Start the note
+        m = midi::on { 0, midi_message[1], midi_message[2] };
+      else if (hs == 8 //< Note-off status
+               // But a note-on with a 0-velocity means also a note-off
+               || (hs == 9 && midi_message[2] == 0))
+        // Stop the note
+        m = midi::off { 0, midi_message[1], midi_message[2] };
+    }
   }
   return m;
 }
