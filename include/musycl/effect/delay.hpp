88fd0bb1ffb7d7f23e80681053c16ffd9e40330e
Ronan Keryell <ronan@keryell.fr>
2022-04-23 10:12:49 -0700

    Use a parallel_for SYCL kernel for the delay effect

--- include/musycl/effect/delay.hpp
+++ include/musycl/effect/delay.hpp
@@ -20,6 +20,8 @@ class delay {
  public:
   // Keep 5 seconds of delay
   static constexpr auto frame_delay = static_cast<int>(5 * frame_frequency);
+  // Size of the delay line
+  static constexpr auto delay_size = frame_delay * frame_size;
 
   /// Almost a 8th note of delay by default at 120 bpm sounds cool
   float delay_line_time = 0.245;
@@ -29,34 +31,43 @@ class delay {
 
  private:
   // The buffer implementing the delay line on the accelerator
-  sycl::buffer<audio::sample_type> delay { frame_delay * frame_size };
+  sycl::buffer<audio::sample_type> delay { delay_size };
 
 
  public:
-  /// Process an audio frame
+  /**  Process an audio frame
+
+       \param[inout] 1 audio frame which is processed
+  */
   void process(audio::frame& audio) {
-    // Input-audio audio with 1 audio frame
-    sycl::buffer<trisycl::vec<double, 2>> input_output { audio };
-    sycl::queue {}.submit([&](auto cgh) {
+    // Make a buffer from the audio frame so it can processed from a SYCL kernel
+    sycl::buffer input_output { audio };
+    // Delay shift in term of sample number
+    int shift = delay_line_time * sample_frequency;
+    // Submit a command group to the default device
+    sycl::queue {}.submit([&](auto& cgh) {
+      // Request a read-write access to the audio frame on the device
       sycl::accessor io { input_output, cgh };
+      // Request a read-write access to the delay buffer on the device
       sycl::accessor d { delay, cgh };
-      static_assert(io.access_mode() == sycl::access::mode::read_write);
-      cgh.single_task([=] {
-        std::shift_left(d.begin(), d.end(), frame_size);
-        std::ranges::copy(io, d.end() - frame_size);
-        int shift = delay_line_time * sample_frequency;
+      // The delay processing kernel to run on the device
+      cgh.parallel_for(frame_size, [=](std::size_t i) {
+        // Shift the delay line by a frame towards the beginning,
+        // strip-mined by frame. \todo Use SYCL scoped parallelism or
+        // introduce algorithms with ad-hoc execution policy
+        for (int sample = 0; sample < delay_size - frame_size;
+             sample += frame_size)
+          d[sample + i] = d[sample + frame_size + i];
+        // Copy the audio frame to the end of the delay line
+        d.rbegin()[i] = io.rbegin()[i];
         // Left channel
-        auto f = ranges::subrange(d.end() - frame_size - shift,
-                                  d.end() - shift);
-        for (auto&& [a, delayed] : ranges::views::zip(io, f))
-          a.x() += delayed.x() * delay_line_ratio;
+        io.rbegin()[i].x() += d.rbegin()[shift + i].x() * delay_line_ratio;
         // Right channel with twice the delay
-        f = ranges::subrange(d.end() - frame_size - 2 * shift,
-                             d.end() - 2 * shift);
-        for (auto&& [a, delayed] : ranges::views::zip(io, f))
-          a.y() += delayed.y() * delay_line_ratio;
+        io.rbegin()[i].y() += d.rbegin()[2 * shift + i].y() * delay_line_ratio;
       });
     });
+    /* The buffer destruction cause the data to be transferred from
+       the device and be copy backed to the audio frame */
   }
 };
 
